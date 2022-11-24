10292e1cfa45f16eea9e321ca657ad37611cef61
slava0135 <super.novalskiy_0135@inbox.ru>
2022-11-24 10:25:23 +0300

    export layout as grid

--- main.go
+++ main.go
@@ -13,11 +13,13 @@ func main() {
 	log.Info("starting up")
 	rand.Seed(time.Now().UnixNano())
 
-	port := ":8080"
 	http.HandleFunc("/generate", handlers.GenerateGameLayout)
 	http.HandleFunc("/generate/image", handlers.GenerateGameLayoutImage)
+	http.HandleFunc("/generate/grid", handlers.GenerateGameLayoutGrid)
 	http.HandleFunc("/validate", handlers.ValidateShipPlacement)
 	http.HandleFunc("/replay", handlers.ReplayGame)
+
+	port := ":8080"
 	log.Info("listening on port ", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
