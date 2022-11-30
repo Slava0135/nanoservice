7b38de22f923f378e22aa4b085d2d6af4cfdc7fd
slava0135 <super.novalskiy_0135@inbox.ru>
2022-11-30 11:59:45 +0300

    less duplication

--- layout/layout_test.go
+++ layout/layout_test.go
@@ -8,24 +8,22 @@ import (
 	"testing"
 )
 
+type linkedSquaresTest struct {
+	name string
+	args Point
+	want []Point
+}
+
 func TestLinkedCornerSquares(t *testing.T) {
-	type args struct {
-		x uint
-		y uint
-	}
-	tests := []struct {
-		name string
-		args args
-		want []Point
-	}{
-		{"zero corner", args{0, 0}, []Point{{1, 1}}},
-		{"n corner", args{rules.N - 1, rules.N - 1}, []Point{{8, 8}}},
-		{"side y", args{1, 0}, []Point{{2, 1}, {0, 1}}},
-		{"side x", args{0, 1}, []Point{{1, 0}, {1, 2}}},
+	tests := []linkedSquaresTest{
+		{"zero corner", Point{0, 0}, []Point{{1, 1}}},
+		{"n corner", Point{rules.N - 1, rules.N - 1}, []Point{{8, 8}}},
+		{"side y", Point{1, 0}, []Point{{2, 1}, {0, 1}}},
+		{"side x", Point{0, 1}, []Point{{1, 0}, {1, 2}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := LinkedCornerSquares(tt.args.x, tt.args.y); !reflect.DeepEqual(got, tt.want) {
+			if got := LinkedCornerSquares(tt.args.X, tt.args.Y); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("LinkedCornerSquares() = %v, want %v", got, tt.want)
 			}
 		})
@@ -33,23 +31,15 @@ func TestLinkedCornerSquares(t *testing.T) {
 }
 
 func TestLinkedSideSquares(t *testing.T) {
-	type args struct {
-		x uint
-		y uint
-	}
-	tests := []struct {
-		name string
-		args args
-		want []Point
-	}{
-		{"zero corner", args{0, 0}, []Point{{1, 0}, {0, 1}}},
-		{"n corner", args{rules.N - 1, rules.N - 1}, []Point{{8, 9}, {9, 8}}},
-		{"side y", args{1, 0}, []Point{{0, 0}, {2, 0}, {1, 1}}},
-		{"side x", args{0, 1}, []Point{{0, 0}, {1, 1}, {0, 2}}},
+	tests := []linkedSquaresTest{
+		{"zero corner", Point{0, 0}, []Point{{1, 0}, {0, 1}}},
+		{"n corner", Point{rules.N - 1, rules.N - 1}, []Point{{8, 9}, {9, 8}}},
+		{"side y", Point{1, 0}, []Point{{0, 0}, {2, 0}, {1, 1}}},
+		{"side x", Point{0, 1}, []Point{{0, 0}, {1, 1}, {0, 2}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := LinkedSideSquares(tt.args.x, tt.args.y); !reflect.DeepEqual(got, tt.want) {
+			if got := LinkedSideSquares(tt.args.X, tt.args.Y); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("LinkedCornerSquares() = %v, want %v", got, tt.want)
 			}
 		})
@@ -57,23 +47,15 @@ func TestLinkedSideSquares(t *testing.T) {
 }
 
 func TestLinkedSquares(t *testing.T) {
-	type args struct {
-		x uint
-		y uint
-	}
-	tests := []struct {
-		name string
-		args args
-		want []Point
-	}{
-		{"zero corner", args{0, 0}, []Point{{1, 1}, {1, 0}, {0, 1}}},
-		{"n corner", args{rules.N - 1, rules.N - 1}, []Point{{8, 8}, {8, 9}, {9, 8}}},
-		{"side y", args{1, 0}, []Point{{2, 1}, {0, 1}, {0, 0}, {2, 0}, {1, 1}}},
-		{"side x", args{0, 1}, []Point{{1, 0}, {1, 2}, {0, 0}, {1, 1}, {0, 2}}},
+	tests := []linkedSquaresTest{
+		{"zero corner", Point{0, 0}, []Point{{1, 1}, {1, 0}, {0, 1}}},
+		{"n corner", Point{rules.N - 1, rules.N - 1}, []Point{{8, 8}, {8, 9}, {9, 8}}},
+		{"side y", Point{1, 0}, []Point{{2, 1}, {0, 1}, {0, 0}, {2, 0}, {1, 1}}},
+		{"side x", Point{0, 1}, []Point{{1, 0}, {1, 2}, {0, 0}, {1, 1}, {0, 2}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := LinkedSquares(tt.args.x, tt.args.y); !reflect.DeepEqual(got, tt.want) {
+			if got := LinkedSquares(tt.args.X, tt.args.Y); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("LinkedCornerSquares() = %v, want %v", got, tt.want)
 			}
 		})
@@ -81,16 +63,7 @@ func TestLinkedSquares(t *testing.T) {
 }
 
 func TestLayout_GridParsing(t *testing.T) {
-	l := Layout{}
-	
-	l[3][3] = true
-	l[3][4] = true
-
-	l[8][8] = true
-	
-	l[1][0] = true
-	l[2][0] = true
-	l[3][0] = true
+	l := simpleLayout()
 	
 	s := l.String()
 	pl, _, err := ParseLayout(s[:len(s)-1])
@@ -103,16 +76,7 @@ func TestLayout_GridParsing(t *testing.T) {
 }
 
 func TestLayout_ShipParsing(t *testing.T) {
-	l := Layout{}
-	
-	l[3][3] = true
-	l[3][4] = true
-
-	l[8][8] = true
-	
-	l[1][0] = true
-	l[2][0] = true
-	l[3][0] = true
+	l := simpleLayout()
 	
 	ships := ShipsFromLayout(l)
 
@@ -128,4 +92,19 @@ func TestLayout_ShipParsing(t *testing.T) {
 	if reflect.DeepEqual(l, pl) != true {
 		t.Fatalf("initial and parsed layouts should be the same:\n%v\n%v", l, pl)
 	}
+}
+
+func simpleLayout() Layout {
+	l := Layout{}
+	
+	l[3][3] = true
+	l[3][4] = true
+
+	l[8][8] = true
+	
+	l[1][0] = true
+	l[2][0] = true
+	l[3][0] = true
+
+	return l
 }
